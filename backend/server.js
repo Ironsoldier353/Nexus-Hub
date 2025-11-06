4f8d991b06414d43f6a425d614d94eeef13ea0ee
Sayan Das <sayandasraja56@gmail.com>
2025-11-06 16:31:28 +0530

    url for deployment client

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://nexus-hub-lilac.vercel.app',
     credentials: true,
     optionSuccessStatus: 200
 }));
